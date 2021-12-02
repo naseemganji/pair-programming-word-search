6075eae7ee3e75e18f5538bc8734ca4af39d04c9
Naseem Ganji <naseem.ganji@icloud.com>
2021-12-01 20:13:40 -0500

    wordsearch.js updated and tested

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map((ls) => ls.join(""));
+    for (let l of horizontalJoin) {
+        if (l.includes(word) || l.includes(word.split("").reverse().join(""))) {
+            return true;
+        }
+
+        let verticalJoin = transpose(letters).map((ls) => ls.join(""));
+        for (let l of verticalJoin) {
+            if (l.includes(word) || l.includes(word.split("").reverse().join(""))) {
+                return true;
+            }
+        }
     }
-}
+        return false;
+    };
+
+    const transpose = function(matrix) {
+        let output = [];
+        for (let i = 0; i < matrix[0].length; i++) {
+            const subOutput = [];  
+            for (let j = 0; j < matrix.length; j++) {
+                subOutput.push(matrix[j][i]);
+            }
+            output.push(subOutput);
+        }
+        return output;
+    };
+
+    ////////////////////////////////////////
 
 module.exports = wordSearch
